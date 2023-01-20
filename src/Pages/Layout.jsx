24b0d1a03d79c9fdbe473ab17966789674e36e22
ajadreddy007 <ajad.edavelly@testaing.com>
2023-01-20 09:48:24 +0530

    commit

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import './Layout.css'
+import axios from 'axios'
+const Layout = () => { 
+    const [shows, setShows] = useState([])
 
-const Layout = () => {
+    const fetchShows = () => {
+        axios.get('https://api.tvmaze.com/search/shows?q=all#')
+            .then((res) => {
+                // console.log(res.data)
+                setShows(res.data)
+            })
+            .catch((err) => {
+                console.log(err)
+            });
+    };
+    useEffect(() => {
+        fetchShows()
+    }, []);
   return (
-    <div>
-        <p>HI</p>
+    <>
+    <h1 style={{paddingLeft:'40px'}}>TV Shows</h1>
+        <div className='cards'>
+        
+        {
+            shows.map((movie)=>{
+                return(
+                    <div className='movie-card'>
+                        <img  src={movie.show.image.original} alt="" />
+                        <h2>{movie.show.name}</h2>
+                        <p>{movie.show.genres[0]}/{movie.show.genres[1]}</p>
+                        <a href="/">Summary</a>
+                    </div>    
+                )
+            })
+        }
     </div>
+    </>
+    
   )
 }
 
